ee984aeafae756c7ad1e138703eca8aceb956a01
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 18:06:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,6 @@
 
 def key_for_min_value(name_hash)
 
-  hash = name_hash.sort.map{|k,v| "#{k.inspect}=>#{v.inspect}"}.join(", ")
-  hash.sort_by{ |key, value| key }.first
+  name_hash.sort_by{ |key, value| key }.first
 
 end
