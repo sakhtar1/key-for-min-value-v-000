f57863b5fd0b2a1162bef16ecaa55626c237fed4
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 16:39:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,10 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    
-    puts "#{value}"
-end
+    if a.values.min_by(&:last)
+      puts "#{value}"
+    else
+      nil
+    end
+  end
 end
