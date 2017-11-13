91b6305cb1d21a05585b29d8feefe15aad563d55
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 19:45:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
 
   if name_hash.sort_by.map do |key,value|
     || name_hash.sort_by.collect do |key,value|
-      return key
-    end
+        return key
+      end
     end
   else
     nil
