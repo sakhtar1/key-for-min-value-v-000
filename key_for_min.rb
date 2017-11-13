ef27908ef9a6f6c0cb71b819d5e77249873953d2
Sharmina Akhtar <sakht318@gmail.com>
2017-11-13 16:40:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    if a.values.min_by(&:last)
+    if a.sort_by { |key, value| value }.first
       puts "#{value}"
     else
       nil
